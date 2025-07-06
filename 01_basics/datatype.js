0d111bfa3abc367bd60d2187cb4853951e5f788e
Jyotsana Rai <148875123+jyotsana8@users.noreply.github.com>
2025-07-06 16:14:34 +0000

    Add JavaScript basics: var.js,datatype.js

---
+++ 01_basics/datatype.js
@@ -0,0 +1,3 @@
+"use strict";// it's contain new version of JS,treat all code as new version of js
+//alert (3+3) use for msg pop up , use in browser 
+// use ecma script for learning js module , mdn website is easy to learn js
---
+++ 01_basics/var.js
@@ -0,0 +1,10 @@
+let score=33
+const accountId=12587
+var name="Jyotsana"
+let address
+console.log(accountId);
+console.table([accountId,name,score, address])
+/* Note:
+1. Do not use var because it's does not contain block scope as well as functional scope
+2. use console.table for printing multiple values
+3. const value never change*/
